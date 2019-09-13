83992f1bb11a601088bcaf7239c8a219f5a515e7
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-13 18:40:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(string_here)
+ 
+ new2 =[]
+ i =0
+ while i < string_herelength
+ new1.each do |word|
+   word = word.length
+   new2 << word
+   i +=1
+ end
+end
+p new2.sort
+end
