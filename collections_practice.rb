2a8879c17c823429fbcf2c14be809cfdd55f775b
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-13 18:45:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,16 +6,6 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
-def sort_array_char_count(string_here)
- 
- new2 =[]
- i =0
- while i < string_herelength
- new1.each do |word|
-   word = word.length
-   new2 << word
-   i +=1
- end
-end
-p new2.sort
+def sort_array_char_count(arr)
+arr.sort
 end
